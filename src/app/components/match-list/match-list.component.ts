3087e7ef3a272f23fea2b21c08eeb6236ee9a822
Amit Pal <amit.pal@appdynamics.com>
2020-07-24 23:05:34 +0530

    feat: 🎸 processed api response and created styles

--- src/app/components/match-list/match-list.component.ts
+++ src/app/components/match-list/match-list.component.ts
@@ -1,23 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
+import { Observable, timer } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
 
 import { MatchService } from '../../containers/match/match.service';
 
 @Component({
   selector: 'app-match-list',
   templateUrl: './match-list.component.html',
-  styleUrls: ['./match-list.component.scss']
+  styleUrls: ['./match-list.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom // to apply CSS on injected HTML contents
 })
-export class MatchListComponent implements OnInit {
+export class MatchListComponent implements OnInit, OnDestroy {
 
-  matches: Observable<string>;
+  public matches: Observable<string>;
+  private alive: boolean;
+  private interval: number = 60 * 2 * 1000;
 
   constructor(private matchService: MatchService) {
   }
 
   ngOnInit(): void {
-    this.matches = this.matchService.matches;
-    this.matchService.loadAllMatches();
+    this.alive = true;
+    timer(0, this.interval)
+      .pipe(
+        takeWhile(() => this.alive)
+      )
+      .subscribe(() => {
+        this.matches = this.matchService.matches;
+        this.matchService.loadAllMatches();
+      });
   }
 
+  ngOnDestroy(): void {
+    this.alive = false;
+  }
 }
--- src/app/containers/match/match.service.ts
+++ src/app/containers/match/match.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import * as $ from 'jquery';
 
 @Injectable()
 export class MatchService {
-  private $$matches = new BehaviorSubject<any>('');
+  private $$matchOberserver = new BehaviorSubject<any>('');
   private baseUrl = '/api/matches';
   private dataStore: { matches: string } = { matches: '' };
-  public readonly matches = this.$$matches.asObservable();
+  public readonly matches = this.$$matchOberserver.asObservable();
 
   constructor(private http: HttpClient) {}
 
@@ -17,13 +18,14 @@ export class MatchService {
       .get(this.baseUrl, { responseType: 'text' })
       .pipe(
         catchError((err) => {
-          this.$$matches.next('<span>No matches found</span>');
+          this.$$matchOberserver.next('<span>No matches found</span>');
           return throwError(err);
         })
       )
       .subscribe((data) => {
+        $(data).append('<p>amit</p>');
         this.dataStore.matches = data;
-        this.$$matches.next(data);
+        this.$$matchOberserver.next(data);
       });
   }
 }
